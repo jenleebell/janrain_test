72b9e4e315e168d2067848c03c3ddb2c95b0bc36
Jennifer <jenleebell@gmail.com>
2016-03-17 11:38:51 -0700

    Fix token URL link, add rpx helper

--- app/controllers/social_callback_controller.rb
+++ app/controllers/social_callback_controller.rb
@@ -1,4 +1,11 @@
 class SocialCallbackController < ApplicationController
+  include Rpx
+  skip_before_action :verify_authenticity_token, only: :engage, if: :json_request?
+
   def social
   end
+
+  def json_request?
+  request.format.json?
+end
 end
