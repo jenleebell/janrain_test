ccdf52076bb51f67ce8ceaa234b25462b4a0e15d
Jennifer <jenleebell@gmail.com>
2016-03-17 11:21:34 -0700

    Implement janrain javascript and div element

---
+++ app/controllers/social_callback_controller.rb
@@ -0,0 +1,4 @@
+class SocialCallbackController < ApplicationController
+  def social
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'sessions#main'
+
+  get 'social_callback' => 'sessions#main'
+  post 'social_callback' => 'social_callback#social'
 end
